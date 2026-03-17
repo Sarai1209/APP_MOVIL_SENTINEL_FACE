83ef0aa85ed96d37c4d71763856f08e54ba56eb2
sarai1209 <sofiadiaz3109@gmail.com>
2026-03-16 23:34:44 -0500

    style: añadir paleta a theme

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,29 +1,30 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 import { Platform } from 'react-native';
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
+const pinkNeon = '#FF00FF'
+const purpleNeon = '#9D00FF'
+const backgroundDark = '#050505'
 
 export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
-    tint: tintColorLight,
+    tint: '#0a7ea4',
     icon: '#687076',
     tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    tabIconSelected: '#0a7ea4',
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
+    text: '#FFFFFF',
+    background: backgroundDark,
+    tint: pinkNeon,
     icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    tabIconDefault: '#444455',
+    tabIconSelected: pinkNeon,
+    pinkNeon: pinkNeon,
+    purpleNeon: purpleNeon ,
+    carBg: '#121225',
   },
 };
 
